ad9f3be9afe682105dc88dce0b45e938b9bc5d59
Ekaterina Mironenko <Krivets86@mail.ru>
2020-12-19 16:00:16 +0300

    3. Прерывание нити [#433172]

---
+++ src/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,30 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    public static void main(String[] args) {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        try {
+            Thread.sleep(6000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        progress.interrupt();
+    }
+    @Override
+    public void run() {
+        String[] process = {"_", "_\\", "_\\|", "_\\|/", "_\\|/_", ""};
+        int i = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println("\nLoading is interrupted!");
+                break;
+            }
+            System.out.print("\r load: " + process[i++]);
+            if (i == 6)
+                i = 0;
+        }
+    }
+}
