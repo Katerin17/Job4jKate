9cc61162d930e0621c84632902ad7a8e6539d439
Ekaterina Mironenko <Krivets86@mail.ru>
2021-01-02 19:44:21 +0300

    3. Junit тест для блокирующей очереди. [#433163]

--- src/test/java/SimpleBlockingQueueTest.java
+++ src/test/java/SimpleBlockingQueueTest.java
@@ -1,12 +1,17 @@
 import org.junit.Test;
 import ru.job4j.SimpleBlockingQueue;
 
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class SimpleBlockingQueueTest {
     @Test
     public void queueShouldBeEmpty() throws InterruptedException {
-        SimpleBlockingQueue<Integer> sbq = new SimpleBlockingQueue<>(2);
+        SimpleBlockingQueue<Integer> sbq = new SimpleBlockingQueue<>(5);
         Thread first = new Thread(() -> {
             for (int i = 0; i < 20; i++) {
                 sbq.offer(i);
@@ -26,6 +31,32 @@ public class SimpleBlockingQueueTest {
         first.join();
         second.join();
         assertEquals(sbq.getQueueSize(), 0);
+    }
 
+    @Test
+    public void allDataShouldBeFetch() throws InterruptedException {
+        CopyOnWriteArrayList<Integer> list = new CopyOnWriteArrayList<>();
+        SimpleBlockingQueue<Integer> sbq = new SimpleBlockingQueue<>(5);
+        Thread producer = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                sbq.offer(i);
+            }
+        });
+        Thread consumer = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted() || sbq.getQueueSize() != 0) {
+                try {
+                    list.add(sbq.poll());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(list, is(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)));
     }
 }
